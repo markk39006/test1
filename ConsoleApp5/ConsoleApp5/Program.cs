8ba530d26d652560d1e6a82d23219f35628ea4c5
markk39006 <markk39006@gmail.com>
2021-03-28 14:28:26 +0700

    test1

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int x = a; int y = b;
+            while (x != y)
+            {
+                if (x < y == true)
+                {
+                    x = x + a;
+                }
+                else if (x < y == false)
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
